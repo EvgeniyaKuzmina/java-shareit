84cb36f324fefec25abd9058780b72d5d3df5057
Evgeniya <vlasova.jen@mail.ru>
2022-08-08 11:37:53 +0300

    feat: подгоняю под тесты

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -8,7 +8,9 @@ import java.util.Collection;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     Collection<Booking> findAllByBookerIdOrderByStartDesc(Long bookerId);
+
     Collection<Booking> findAllByBookerIdOrderByStartAsc(Long bookerId);
+    
     Collection<Booking> findAllByItemIdOrderByStartDesc(Long itemId);
 
 }
